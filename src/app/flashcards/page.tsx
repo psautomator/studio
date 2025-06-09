de79ccfce7c0506620bda99f20467bdd272da21f
Urrel Mozes <EarlFM@gmail.com>
2025-06-09 16:59:00 +0000

    i see a lot of missing pages. make sure the corrrect pages are presented

--- src/app/flashcards/page.tsx
+++
@@ -1,6 +0,0 @@
-// This file is intentionally left blank.
-// It is being removed to fix a build error related to i18n routing.
-// The correct flashcards page is located at src/app/[locale]/flashcards/page.tsx.
-//
-// If you see this comment, the file deletion operation might not have completed as expected.
-// Please ensure this file is removed from your project.
